e4bc625df413e4a90705eb9e341f8b47478eedf4
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-07 17:03:11 -0800

    (feat) Crate reexports.

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#![license = "Mozilla Publice License, v. 2.0"]
+#![deny(missing_docs, warnings)]
+
+//! A load balancing TaskPool and WorkQueue.
+//!
+//! Extracted from Cargo and Servo for general use.
+
+extern crate rand;
+
+pub use pool::TaskPool;
+pub use queue::WorkQueue;
+
+pub mod pool;
+pub mod queue;
+
